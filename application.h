ee3f49b219254a3107d368a7482f15f542492042
duartepbrandao <duartepbrandao@hotmail.com>
2014-10-31 12:10:27 -0700

    git start app

---
+++ application.h
@@ -0,0 +1,20 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+
+struct app_layer {
+	int status;
+	int port;
+	int serialPortDescriptor;
+	int packetSize;
+	int fileDescriptor;
+	int fileSize;
+	unsigned char currentNum;
+	unsigned char name[PATH_MAX];
+} typdef app_layer_T;
+
+int setup();
+int sendFile();
+int receiveFile();
--- main.c
+++ main.c
@@ -5,32 +5,12 @@
 #include <stdio.h>
 
 
-int mode = 0;
-void print_usage(char* program){
-	 ERROR("Usage:");
-	 ERRORF("\t- %s /dev/ttyS0 send|rec <filename>", program);
+
 }
 
 
 int main(int argc, char argv[]) {
 
-	if (argc != 4 || ((strcmp("/dev/ttyS0", argv[1]) != 0))) {
-		print_usage(argv[0]);
-		return EXIT_FAILURE;
-	}
-	else if(strcmp(argv[2], "send") == 0 || strcmp(argv[2], "rec") == 0){
-		print_usage(argv[0]);
-		return EXIT_FAILURE;
-	}
-
-	mode = strcmp(argv[2],"send");
-
-	if(mode){
-		//receber
-	}
-	else{
-		//enviar
-	}
-
+	setup();
 	return EXIT_SUCESS;
 }
